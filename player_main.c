2619f1a1ba49a7588fa49ccf6f896781ae4ed26f
Grace Gomez <57299249+grace-gomez-ttu@users.noreply.github.com>
2019-11-05 14:59:29 -0600

    Create player_main.c

---
+++ player_main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <malloc.h>
+#include <iostream>
+#include <fstream> 
+#include <string.h>
+
+#define MAX 50
+
+using namespace std;
+
+#include "stacks.h"
+//#include "queue.h"
+#include "player.h"
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	cout << sizeof(player);
+	//create_line_up(8);
+	return 0;
+}
