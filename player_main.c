5ee11b4b86117bab8e55b901a0f39e93195fdd41
Grace Gomez <57299249+grace-gomez-ttu@users.noreply.github.com>
2019-11-08 14:45:27 -0600

    Update player_main.c

--- player_main.c
+++ player_main.c
@@ -14,19 +14,24 @@ using namespace std;
 #include "stacks.h"
 //#include "queue.h"
 #include "player.h"
+#include "dice.h"
+
+struct stack* arrows;
+struct player_queue lineup;
+bool savethesheriff;
 
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
 	cout << sizeof(player);
-	struct player_queue line_up = create_line_up(8);
-	print_recursive(line_up,line_up.front,line_up.front);
-	while((line_up front != 1) || (sides <= 1) || (line_up.front == line_up.rear)){
+	lineup = create_line_up(8);
+	print_recursive(lineup,lineup.front,lineup.front);
+	//while((line_upfront != 1) || (sides <= 1) || (line_up.front == line_up.rear)){
 		
-	}
-	cout << "The winner is: "
-	print_recursive(line_up,line_up.front,line_up.front);
+	}//
+	cout << "The winner is: " ;
+	print_recursive(lineup,lineup.front,lineup.front);
 	return 0;
 }
 
-when a character dies sides = sides - temp_player->role.
+//when a character dies sides = sides - temp_player->role.
