72f5934cdc55671f7336789cf9ee4c27da9780ce
Grace Gomez <57299249+grace-gomez-ttu@users.noreply.github.com>
2019-11-13 17:35:22 -0600

    Update player_main.c

--- player_main.c
+++ player_main.c
@@ -14,21 +14,51 @@ using namespace std;
 #include "stacks.h"
 //#include "queue.h"
 #include "player.h"
-#include "dice.h"
 
 struct stack* arrows;
 struct player_queue lineup;
-bool savethesheriff;
+
+#include "dice22.h"
+
+/*{ Library functions table of content 
+
+stacks.h
+create_stack();
+push(int, stack)
+pop(stack)
+isEmpty(stacks);
+isFull(stack)
+
+player.h
+struct player_queue create_player_queue();                         //to create a queue in another function call using { struct queuec* [the name of your queue] = create_queuec }
+bool isEmpty_player_queue (struct player_queue varq);              // Function that checks if the player_queue is empty
+bool isFull_player_queue (struct player_queue varq);               // Function that checks if the player_queue is full
+void enqueue_player (struct player_queue* varq, struct player);    // Function that adds a player to the player_queue
+void dequeue_player (struct player_queue* varq);                   // Function that deletes a player from the player_queue
+void peek_player (struct player_queue varq);                       // Function that prints out the name ofthe current player playing
+
+dice.h
+struct stack dice_sheriff(int current, bool* gatlin, bool* dynamite, int* beer);
+struct stack dice_deputy(int current, bool* gatlin, bool* dynamite, int* beer);
+struct stack dice_outlaw(int current, bool* gatlin, bool* dynamite, int* beer);
+bool save_the_sheriff();
+bool pull_arrow(int current);
+void fill_arrows();
+void indian_attack(int current, int start); )*/
+
 
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
 	cout << sizeof(player);
-	lineup = create_line_up(8);
+	lineup = create_line_up(8);//(srand(time(0))%5+4);
+	struct player_queue debugg = lineup;
+	arrows = create_stack();
 	print_recursive(lineup,lineup.front,lineup.front);
+	//struct stack shot = dice_sheriff(0,&g,&d&b);
 	//while((line_upfront != 1) || (sides <= 1) || (line_up.front == line_up.rear)){
 		
-	}//
+	//}
 	cout << "The winner is: " ;
 	print_recursive(lineup,lineup.front,lineup.front);
 	return 0;
